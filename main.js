b68ffc8e06ee6069d0c6a1bc5eecc76fd861085e
Ryan Kaszubski <47284437+ryanksz@users.noreply.github.com>
2021-02-02 21:50:30 -0500

    cleaned up the code

--- main.js
+++ main.js
@@ -4,9 +4,20 @@ function getCurrentTime() {
   let hour = time.getHours(); 
   let min = time.getMinutes(); 
   let sec = time.getSeconds(); 
+
+  min = lessThanTen(min);
+  sec = lessThanTen(sec);
   
   document.getElementById("time").innerHTML = hour + ":"  + min + ":" + sec; 
 } 
+
+function lessThanTen(t) {
+  if (t < 10) {
+    return "0" + t;
+  }else{
+    return t;
+  }
+}
 
 
   getCurrentTime();
